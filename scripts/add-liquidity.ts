13f3463cdd059ed792a6e801318f977e7473f687
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-13 16:09:00 +0100

    Updated interaction proof image

--- scripts/add-liquidity.ts
+++ scripts/add-liquidity.ts
@@ -6,6 +6,7 @@ async function main() {
     const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const USDC_DAI_PAIR = "0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5";
 
     const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
 
@@ -20,6 +21,7 @@ async function main() {
 
     const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const DAI_Contract = await ethers.getContractAt("IERC20", DAI, impersonatedSigner);
+    const LP_COntracts = await ethers.getContractAt("IERC20", USDC_DAI_PAIR, impersonatedSigner); 
     
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
 
@@ -34,6 +36,10 @@ async function main() {
 
     console.log("usdc balance before swap", Number(usdcBal));
     console.log("dai balance before swap", Number(daiBal));
+
+    // Check LP token balance before the transaction
+    const lpBalBefore = await LP_COntracts.balanceOf(impersonatedSigner.address);
+    console.log("LP Token Balance before:", Number(lpBalBefore));
 
     const addLiqTx = await ROUTER.addLiquidity(
         USDC,
@@ -54,6 +60,13 @@ async function main() {
 
     console.log("usdc balance after liquidity", Number(usdcBalAfter));
     console.log("dai balance after liquidity", Number(daiBalAfter));
+
+    console.log("=========================================================");
+
+    // Check LP token balance before the transaction
+    const lpBalAfter = await LP_COntracts.balanceOf(impersonatedSigner.address);
+    console.log("LP Token Balance before:", Number(lpBalAfter));
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
